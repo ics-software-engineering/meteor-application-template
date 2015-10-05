55b47fe9fa499c4e2429fa2f8b4667be4eeba463
Philip Johnson <johnson@hawaii.edu>
2015-10-05 12:04:10 -0700

    Add code for image problem

--- app/lib/router/Router.js
+++ app/lib/router/Router.js
@@ -2,6 +2,8 @@
  * Configure Iron Router.
  * See: http://iron-meteor.github.io/iron-router/
  */
+
+
 Router.configure({
   layoutTemplate: 'Layout',
   waitOn: function() { return Meteor.subscribe("Stuff"); },
--- app/server/seeds/Accounts.js
+++ app/server/seeds/Accounts.js
@@ -1,6 +1,6 @@
 /* When running app for first time, pass a settings file to set up a default user account. */
 if (Meteor.users.find().count() === 0) {
-  if (Meteor.settings.defaultAccount.username) {
+  if (!!Meteor.settings.defaultAccount) {
     Accounts.createUser({
       username: Meteor.settings.defaultAccount.username,
       password: Meteor.settings.defaultAccount.password
