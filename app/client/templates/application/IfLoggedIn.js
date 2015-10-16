88f92c5a63c230476c2192bf481c7625d8d55dd5
Philip Johnson <johnson@hawaii.edu>
2015-10-15 14:38:53 -1000

    Added some JSDoc

--- app/client/templates/application/IfLoggedIn.js
+++ app/client/templates/application/IfLoggedIn.js
@@ -1,7 +1,13 @@
 Template.IfLoggedIn.helpers({
+  /**
+   * @returns {*} True if Meteor is in the process of logging in.
+   */
   authInProcess: function() {
     return Meteor.loggingIn();
   },
+  /**
+   * @returns {boolean} True if there is a logged in user.
+   */
   canShow: function() {
     return !!Meteor.user();
   }
--- app/client/templates/stuff/AddStuff.js
+++ app/client/templates/stuff/AddStuff.js
@@ -4,6 +4,11 @@
  */
 AutoForm.hooks({
   AddStuffForm: {
+    /**
+     * After successful form submission, go to the ListStuff page.
+     * @param formType The form.
+     * @param result The result of form submission.
+     */
     onSuccess: function(formType, result) {
       Router.go('ListStuff');
     }
