30a055e74dd2cf1a64816fa608401f195f62ac89
Philip Johnson <johnson@hawaii.edu>
2016-09-15 13:54:38 -1000

    Migrated to 1.4 and use of imports/ directory.

--- app-new/client/main.js
+++ app-new/client/main.js
@@ -3,3 +3,7 @@ import '/imports/startup/both';
 import '/imports/ui/layouts';
 import '/imports/ui/pages';
 import '/imports/ui/stylesheets/style.css';
+
+// The following is a hack for autoform. Remove it once autoform is removed.
+import { Stuff } from '../imports/api/stuff/Stuff.js';
+window.Stuff = Stuff;
